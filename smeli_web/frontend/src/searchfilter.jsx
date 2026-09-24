23ffb508f19c9a9eae1dda7fecdf2b2a0cedb193
will joransen <wjoransen@brompton-srv.com>
2026-09-24 06:44:56 -0400

    Initial React commit before React branch

---
+++ smeli_web/frontend/src/searchfilter.jsx
@@ -0,0 +1,9 @@
+import React from "react"l
+import { createRoot } from "react-dom/client";
+
+function Hello() {
+
+    return (
+        <h3>HELLO</h3>
+    )
+}
--- smeli_web/routes.py
+++ smeli_web/routes.py
@@ -11,6 +11,18 @@ def index():
 def smeli():
     return render_template("smeli.html")
 
+@smeli_app.route("searchfilter")
+def search_filter():
+    lookup_criteria = dict()
+    for criteria in { 'author', 'title', 'year', 'identified' }:
+        if criteria in request.args:
+            lookup_criteria[criteria] = request.args[criteria]
+
+    if !lookup_criteria:
+        return render_template("searchFilter.html", data=None)
+    candidates = get_paper_candidates( **lookup_criteria )
+    return render_template("searchFilter.html", data=candidates)
+
 @smeli_app.route("/api/lookup")
 def lookup():
     lookup_criteria = dict()
@@ -20,3 +32,4 @@ def lookup():
 
     candidates = get_paper_candidates( **lookup_criteria )
     return jsonify(candidates)
+
